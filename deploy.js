bca8a6680f77a13edaa349b3ebb876ab97f5e70f
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-05-13 20:35:37 +0300

    Connect to Ganache Local Blockchain using JsonRPCProvider

--- deploy.js
+++ deploy.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 
 const abi = fs.readFileSync("SimpleStorage_sol_SimpleStorage.abi","utf8");
 const binary = fs.readFileSync("SimpleStorage_sol_SimpleStorage.bin","utf8");
+// HTTP://127.0.0.1:7545  : RPC URL provided by Ganache
+
+const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545");  // this code connects our script with our blockchain
+
 
 async function main() {
   console.log("some message");
