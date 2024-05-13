a59c8f4fc836945e2766c2d472cd73103ce94d73
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-05-13 22:45:08 +0300

    Add provider, wallet and deploy the contract locally

--- deploy.js
+++ deploy.js
@@ -1,15 +1,24 @@
-const ethers = require("ethers");
+const { ethers } = require("ethers");
 const fs = require("fs");
 
-const abi = fs.readFileSync("SimpleStorage_sol_SimpleStorage.abi","utf8");
-const binary = fs.readFileSync("SimpleStorage_sol_SimpleStorage.bin","utf8");
-// HTTP://127.0.0.1:7545  : RPC URL provided by Ganache
-
-const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545");  // this code connects our script with our blockchain
-
-
 async function main() {
-  console.log("some message");
+  const abi = fs.readFileSync("SimpleStorage_sol_SimpleStorage.abi","utf8");
+  const binary = fs.readFileSync("SimpleStorage_sol_SimpleStorage.bin","utf8");
+  // HTTP://127.0.0.1:7545  : RPC URL provided by Ganache
+
+  let provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545");  // this code connects our script with our blockchain
+  const wallet  = new ethers.Wallet(
+  "0x73eed09c2137aa5ae8e06df7366d924277212457d0487e6c17520a35453caf1d",
+  provider); // a private key obtained from Ganache.
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+
+  console.log("deploying please wait");
+
+  const contract = await contractFactory.deploy();
+
+  console.log(contract);
+
 }
 main()
   .then(() => process.exit(0))
